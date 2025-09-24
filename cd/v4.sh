a3302c430ecb825267f70a4376d548c973abaf76
tsp0610 <tsplegend429@gmail.com>
2025-09-24 13:30:55 +0530

    Update v4.sh

--- cd/v4.sh
+++ cd/v4.sh
@@ -2,14 +2,12 @@
 
 # Benar ASCII Art Banner
 cat << "EOF"
-888b      88               88           88                       
-8888b     88               88           ""    ,d               
-88 `8b    88               88                 88               
-88  `8b   88   ,adPPYba,   88,dPPYba,   88  MM88MMM  ,adPPYYba,  
-88   `8b  88  a8"     "8a  88P'    "8a  88    88     ""     `Y8  
-88    `8b 88  8b       d8  88       d8  88    88     ,adPPPPP88  
-88     `8888  "8a,   ,a8"  88b,   ,a8"  88    88,    88,    ,88  
-88      `888   `"YbbdP"'   8Y"Ybbd8"'   88    "Y888  `"8bbdP"Y8  
+  _______ _____ _____  
+ |__   __/ ____|  __ \ 
+    | | | (___ | |__) |
+    | |  \___ \|  ___/ 
+    | |  ____) | |     
+    |_| |_____/|_|     
 EOF
 
 # Install Tailscale
